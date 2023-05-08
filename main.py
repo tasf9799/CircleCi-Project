6ffd8ec079b19bffdf48443d068c5ed98daa8ba4
tasf9799 <123056351+tasf9799@users.noreply.github.com>
2023-05-08 15:43:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Tauseef Shaikh") #My name 
+def to_upper(name):
+    return name.upper()
+
+
+def say_hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = 'TrainWithShubham'
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
